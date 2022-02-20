c17b59cfe286a96c2d56fdf122e42b2b6ddfa392
Shravan Parigi <shravan.parigi@gmail.com>
2022-02-19 16:43:27 -0800

    adjusted landing page to fit new proposed design; fixed resulting problems; replaced logo file with higher definition logo

--- roast-beef/src/pages/SignIn.js
+++ roast-beef/src/pages/SignIn.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import {NavBar, LandingPage, Button, NextButton, Text, LandingPageWrapper, HeaderText, TextBox} from '../pages/style';
+import { NavBar, LandingPage, Button, NextButton, Text, LandingPageWrapper, HeaderText, TextBox } from '../pages/style';
 import Logo from '../images/logo.png';
 
 const SignIn = () => {
     return (
         <LandingPage>
-            <LandingPageWrapper> 
-                <NavBar> 
-                    <a href='/' className="NavLogo">
+            <NavBar>
+                <a href='/' className="NavLogo">
                     <marquee behavior="alternate" width="300">
-                            <img 
-                            src={Logo} 
-                            alt="Logo" 
+                        <img
+                            src={Logo}
+                            alt="Logo"
                             align="left"
-                            ></img>
-                        </marquee>
-                    </a> 
-                </NavBar> <p/>
-                <LandingPageWrapper>
-                    <HeaderText>LOG IN</HeaderText>
-                    <Text>Enter Your Username: </Text>
-                    <TextBox type="text" placeholder="Username" /><p/>
-                    <Text>Enter Your Password: </Text>
-                    <TextBox type="password" placeholder="Password" /><p/>
-                    <a href='/'>
-                        <NextButton>Back</NextButton>
-                        </a>
-                    <a href='/home'>
-                        <NextButton>Next</NextButton>
-                        </a> <p/>
-                </LandingPageWrapper>
+                            width="200" height="133"
+                        ></img>
+                    </marquee>
+                </a>
+            </NavBar> <p />
+            <LandingPageWrapper>
+                <HeaderText>LOG IN</HeaderText>
+                <Text>Enter Your Username: </Text>
+                <TextBox type="text" placeholder="Username" /><p />
+                <Text>Enter Your Password: </Text>
+                <TextBox type="password" placeholder="Password" /><p />
+                <a href='/'>
+                    <NextButton>Back</NextButton>
+                </a>
+                <a href='/home'>
+                    <NextButton>Next</NextButton>
+                </a> <p />
             </LandingPageWrapper>
         </LandingPage>
     );
