d55f47ec23cf18584f565322138da2afffc8576b
Shravan Parigi <shravan.parigi@gmail.com>
2022-03-03 20:25:28 -0800

    fixed search bar, still opens horizontally though

--- roast-beef/src/pages/Home.js
+++ roast-beef/src/pages/Home.js
@@ -1,35 +1,69 @@
-import {useState, useEffect, React} from 'react';
-import './HomePage.css';
-import db from '../firebase-config';
-import Logo from '../images/logo.png';
-import Popup from './Popup'
-import {NavBar, NavPadding, LandingPage, Button, LandingPageWrapper, Post, PostContents, Text, NextButton, NickyButton, SearchBar, DDButton} from '../pages/style';
-import PostD from './Post';
-import './HomeCss.css'
+import { useState, useEffect, React, useRef } from "react";
+import "./HomePage.css";
+import "./HomeCss.css";
+import {db} from "../firebase-config";
+import Logo from "../images/logo.png";
+import Popup from "./Popup";
+import {
+  NavBar,
+  NavPadding,
+  LandingPage,
+  Button,
+  LandingPageWrapper,
+  Post,
+  PostContents,
+  Text,
+  NextButton,
+  NickyButton,
+  SearchBar,
+  DDButton,
+} from "../pages/style";
+import PostD from "./Post";
+import "./HomeCss.css";
 import "./Popup";
 import { collection } from "firebase/firestore";
 
 const SearchbarDropdown = (props) => {
-    const {options, onInputChange} = props;
-    return(
-        <div className = "outerleft">
-            <SearchBar type="text" placeholder="Search Here" onChange={onInputChange}/> <p/>
-            <ul id = "results">
-                {options.map((option, index) => {
-                    return (
-                        <DDButton
-                            className='list-group-item list-group-item-action'
-                            key={index}
-                        >
-                            {option}
-                        </DDButton>
-                    );
-                })}
-            </ul>
-        </div>
-    );
-}
-
+  const { options, onInputChange } = props;
+  const ulRef = useRef();
+  const inputRef = useRef();
+  useEffect(() => {
+      inputRef.current.addEventListener('click', (event) => {
+        event.stopPropagation();
+        ulRef.current.style.display = 'flex';
+        onInputChange(event);
+      });
+      document.addEventListener('click', (event) =>{
+        ulRef.current.style.display = 'none';
+      });
+  }, []);
+  return (
+    <div className="outerleft">
+      <SearchBar
+        type="text"
+        placeholder="Search Here"
+        ref={inputRef}
+        onChange={onInputChange}
+      />{" "}
+      <p />
+      <ul id="results" ref={ulRef}>
+        {options.map((option, index) => {
+          return (
+            <DDButton
+              className="list-group-item list-group-item-action"
+              key={index}
+              onClick={(e) => {
+                inputRef.current.value = option;
+                }}
+            >
+              {option}
+            </DDButton> 
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 const defaultOptions = [];
 defaultOptions.push(`#political`);
@@ -41,13 +75,13 @@ defaultOptions.push(`#arts`);
 defaultOptions.push(`#history`);
 defaultOptions.push(`#casual`);
 defaultOptions.push(`#ucla`);
-defaultOptions.push(`#computer science`);
+defaultOptions.push(`#computerscience`);
 defaultOptions.push(`#wordle`);
 defaultOptions.push(`#globle`);
 defaultOptions.push(`#handshakes`);
 
-for(let i = 0; i < 10; i++){
-    defaultOptions.push(`tag ${i}`);
+for (let i = 0; i < 10; i++) {
+  defaultOptions.push(`tag ${i}`);
 }
 
 const Home = () => {
@@ -66,38 +100,47 @@ const Home = () => {
       postText: "we hate sank",
     },
   ]);
-  
+
   const postsCollectionRef = collection(db, "posts");
   const [buttonPopup, setButtonPopup] = useState(false);
-
-    const [options, setOptions] = useState([]);
-    const onInputChange = (event) => {
-        console.log(event.target.value);
-        setOptions(
-            defaultOptions.filter((option) => option.includes(event.target.value))
-        );
-    };
+  const [options, setOptions] = useState([]);
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+    setOptions(
+      defaultOptions.filter((option) => option.includes(event.target.value))
+    );
+  };
 
   return (
     <LandingPage>
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup>
-    <NavBar>
-      <br></br>
-      <SearchbarDropdown options={options} onInputChange={onInputChange}/>
-      <div className="outerright">
+      <NavBar>
+        <br></br>
+        <SearchbarDropdown options={options} onInputChange={onInputChange} />
+        <div className="outerright">
           <div>
-              <NickyButton onClick={() => setButtonPopup(true)}> Create Post </NickyButton>
+            <NickyButton onClick={() => setButtonPopup(true)}>
+              {" "}
+              Create Post{" "}
+            </NickyButton>
           </div>
-      </div>
-      <div className = "stayPutHome">
-          <a href='/' className="NavLogo">
-              <marquee behavior="alternate" width="300">
-                  <img src={Logo} alt="Logo" align="left" width="200" height="133" ></img>
-              </marquee>
+        </div>
+        <div className="stayPutHome">
+          <a href="/" className="NavLogo">
+            <marquee behavior="alternate" width="300">
+              <img
+                src={Logo}
+                alt="Logo"
+                align="left"
+                width="200"
+                height="133"
+              ></img>
+            </marquee>
           </a>
-      </div>
-  </NavBar> <p/>
-   <NavPadding></NavPadding> <p />
+        </div>
+      </NavBar>{" "}
+      <p />
+      <NavPadding></NavPadding> <p />
       {posts.map((post) => (
         <PostD
           username1={post.username1}
